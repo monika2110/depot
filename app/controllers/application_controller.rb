d6dc81c937bf67fdd98554b4ec7cd5df5aee42c4
monika2110 <92295303+monika2110@users.noreply.github.com>
2021-12-08 02:10:57 +0100

    1.,2.,3. zadatak

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :set_time
+  def set_time
+    @time = Time.now
+  end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,17 @@
 class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
+
+  def count
+    if session[:counter].nil?
+
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+
   def index
     @products = Product.order(:title)
+    @counter = count
   end
 end
